f2597fec5bf1099010cd49add090c006678ba0bb
abdurahmanab <abdurahmanabdela47@gmail.com>
2023-10-11 09:22:20 +0300

    Repository Pattren

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -91,7 +91,7 @@ class PostController extends Controller
         if (!$post) {
             return response()->json(['message' => 'Post Not Found'], 404);
         }
-    
+     
         // Delete the user.
         $post->forceDelete();
     
