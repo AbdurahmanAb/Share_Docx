325b98cef91b574ff9b0b938536dda6683a96d90
abdurahmanab <abdurahmanabdela47@gmail.com>
2023-10-12 14:21:51 +0300

    + Event

--- app/Events/UserCreated.php
+++ app/Events/UserCreated.php
@@ -2,6 +2,7 @@
 
 namespace App\Events;
 
+use App\Models\User;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
@@ -14,11 +15,13 @@ class UserCreated
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
+    protected $user;
     /**
      * Create a new event instance.
      */
-    public function __construct()
+    public function __construct(User $user)
     {
+        $this->user = $user;
         //
     }
 
