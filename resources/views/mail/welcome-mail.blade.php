b077724eb37a646af5a09cb3dab3278d6a97b9fe
abdurahmanab <abdurahmanabdela47@gmail.com>
2023-10-14 09:32:09 +0300

    +Mail Templete

--- resources/views/mail/welcome-mail.blade.php
+++ resources/views/mail/welcome-mail.blade.php
@@ -1,3 +1,12 @@
 @component('mail::message')
-    # Welcome To Here
-@endcomponent
+    # Welcome To Share Docx
+
+    @component('mail::button', ['url' => 'fb.com'])
+        Verify Account
+    @endcomponent
+    @component('mail::panel')
+        This is Panel
+    @endcomponent
+    @component('mail::footer')
+        This is a footer
+    @endcomponent
