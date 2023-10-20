2482a59c304eeaf9d19b945a3861c95bf76ff239
abdurahmanab <abdurahmanabdela47@gmail.com>
2023-10-20 21:53:09 +0300

    added some commits

--- tests/Unit/PostTest.php
+++ tests/Unit/PostTest.php
@@ -2,15 +2,34 @@
 
 namespace Tests\Unit;
 
+use App\Http\Controllers\v1\PostController;
 use PHPUnit\Framework\TestCase;
 
 class PostTest extends TestCase
 {
+    public $controller = new PostController;
     /**
      * A basic unit test example.
      */
-    public function test_example(): void
+    // public function test_example(): void
+    // {
+    //     $this->assertTrue(true);
+    // }
+    public function test_create()
     {
-        $this->assertTrue(true);
+        $data = [];
+        $create = $this->controller->store($data);
+        //test create Method
+    
+        
+    }
+
+    public function test_update()
+    {
+        
+    }
+    public function test_delete()
+    {
+        
     }
 }
