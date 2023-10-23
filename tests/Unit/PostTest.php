edb789932517817719366879351f66814ac3ed7f
abdurahmanab <abdurahmanabdela47@gmail.com>
2023-10-23 14:01:00 +0300

    added unit testing

--- tests/Unit/PostTest.php
+++ tests/Unit/PostTest.php
@@ -6,11 +6,11 @@ use App\Http\Controllers\v1\PostController;
 use App\Models\Post;
 use App\Http\Requests\StorePostRequest;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
 
-class PostTest extends TestCase
+class PostTest extends Testcase
 {
-    use RefreshDatabase; 
+   use RefreshDatabase; 
   
    
     /**
@@ -20,26 +20,27 @@ class PostTest extends TestCase
     {
         $this->assertTrue(true);
     }
-    // public function testCreate()
-    // {
-    //     $data = [
-    //         'title' => 'TestTitle',
-    //         'body' => 'TestBody'
-    //     ];
-    
-    //     $response = $this->post('/posts', $data);
-    
-    //     $response->assertStatus(201); // Assert the response status code
-    
-    //     $this->assertDatabaseHas('posts', $data); // Assert the data exists in the database
-    // }
+    public function test_create()
+    {
+        $controller = app(PostController::class);
 
-    // public function test_update()
-    // {
+        $data = [
+            'title' => 'TestTitle',
+            'body' => []
+        ];
+    
+        $response = $controller->store(new StorePostRequest(  $data));
+
+        $this->assertSame($data['title'], $response->title, "Assertion Faild");
+        // Assert the data exists in the database
+    }
+
+    public function test_update()
+    {
         
-    // }
-    // public function test_delete()
-    // {
+    }
+    public function test_delete()
+    {
         
-    // }
+    }
 }
